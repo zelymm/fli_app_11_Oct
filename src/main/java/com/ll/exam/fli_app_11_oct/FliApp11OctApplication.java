ac40319e90708d4c9db649075e63df1c795787c4
zelymm <dkdlwkrz@naver.com>
2022-10-11 15:38:33 +0900

    initial commit

---
+++ src/main/java/com/ll/exam/fli_app_11_oct/FliApp11OctApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.fli_app_11_oct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FliApp11OctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FliApp11OctApplication.class, args);
+    }
+
+}
